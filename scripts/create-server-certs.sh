64af74765a2a974ca0ad863d5ca60c0acf2681ff
Jordi Cenzano Ferret <jcenzano@fb.com>
2023-11-14 19:56:12 +0100

    failure with (alleged) ECDSA keys

--- scripts/create-server-certs.sh
+++ scripts/create-server-certs.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # Create certs dir if it does not exists
+rm ../certs/certificate.*
 mkdir -p ../certs
 
-# Generate publi - private key 
-openssl req -newkey rsa:2048 -nodes -keyout ../certs/certificate.key -x509 -out ../certs/certificate.pem -subj '/CN=Test Certificate' -addext "subjectAltName = DNS:localhost"
+# Generate ECDSA public - private key 
+openssl req -new -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -x509 -nodes -days 10 -out ../certs/certificate.pem -keyout ../certs/certificate.key -subj '/CN=Test Certificate' -addext "subjectAltName = DNS:localhost"
+
+# Generate RSA public - private key 
+# openssl req -newkey rsa:2048 -nodes -keyout ../certs/certificate.key -x509 -out ../certs/certificate.pem -subj '/CN=Test Certificate' -addext "subjectAltName = DNS:localhost"
--- scripts/start-localhost-test-chrome.sh
+++ scripts/start-localhost-test-chrome.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# Get base 64 of scert cmd
-certbase64=$(eval "openssl x509 -pubkey -noout -in ../certs/certificate.pem | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | base64")
+# Get base 64 of scert cmd RSA
+# certbase64=$(eval "openssl x509 -pubkey -noout -in ../certs/certificate.pem | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | base64")
+
+# Get base 64 of scert cmd ECDSA
+certbase64=$(eval "openssl x509 -pubkey -noout -in ../certs/certificate.pem | openssl ec -pubin -outform der | openssl dgst -sha256 -binary | base64")
 
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir="/tmp/chrome_dev_session" --enable-features=SharedArrayBuffer --origin-to-force-quic-on=localhost:443  --ignore-certificate-errors-spki-list=$certbase64
